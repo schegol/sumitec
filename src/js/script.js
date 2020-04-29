c90a97bd60e4fa677b0e0046352f7768da0c77ea
schegol <48925789+schegol@users.noreply.github.com>
2020-04-29 17:23:11 +0300

    работа над функционалом и адаптивом

--- src/js/script.js
+++ src/js/script.js
@@ -10,21 +10,45 @@ $(document).ready(function() {
   });
 
   //модалки:
-  const modalTriggers = $('.section__btn, .company-info__modal-trigger, .excavators__modal-trigger');
+  const modalTriggers = $('.section__btn, .company-info__modal-trigger, .excavators__modal-trigger, .quiz__navigation-btn--submit');
   const modalOverlay = $('.landing-modal');
   const modalForm = $('.landing-modal__form');
   const modalCloseBtn = $('.landing-modal__close-btn');
   const modalPhoneField = $('.landing-modal__form-field--phone');
+  const scrollableWrapper = $('.excavators__controls');
 
   modalTriggers.click(function (e) {
-    e.preventDefault();
-    modalOverlay.addClass('landing-modal--open');
-    $('body').addClass('modal-open');
+    if ($(window).width() >= 768) {
+      e.preventDefault();
+      modalOverlay.addClass('landing-modal--open');
+      $('body').addClass('modal-open');
+    } else {
+      if ($(e.target).hasClass('quiz__navigation-btn--submit')) {
+        e.preventDefault();
+        modalOverlay.addClass('landing-modal--open');
+        $('body').addClass('modal-open');
+      } else if ($(e.target).hasClass('excavators__modal-trigger')) {
+        e.preventDefault();
+        scrollableWrapper.removeClass('excavators__controls--center').addClass('excavators__controls--right');
+      } else {
+        e.preventDefault();
+        modalOverlay.addClass('landing-modal--open').addClass('landing-modal--animated');
+        $('body').addClass('modal-open');
+      };
+    };
   });
 
   var closeModal = function () {
-    modalOverlay.removeClass('landing-modal--open');
-    $('body').removeClass('modal-open');
+    if (modalOverlay.hasClass('landing-modal--animated')) {
+      // modalOverlay.addClass('landing-modal--animated-backwards');
+      // setTimeout(function () {
+        modalOverlay.removeClass('landing-modal--open').removeClass('landing-modal--animated').removeClass('landing-modal--animated-backwards');
+        $('body').removeClass('modal-open');
+      // }, 500);
+    } else {
+      modalOverlay.removeClass('landing-modal--open');
+      $('body').removeClass('modal-open');
+    };
   };
 
   modalCloseBtn.click(function () {
@@ -56,29 +80,58 @@ $(document).ready(function() {
   //экскаваторы:
   const excavatorToggles = $('.excavators__item-link');
   const excavatorDetails = $('.excavators__details-wrapper');
+  const excavatorBackBtn = $('.excavators__back-btn');
+  const formBackBtn = $('.landing-modal__back-btn');
 
   excavatorToggles.each(function () {
     $(this).click(function (e) {
       e.preventDefault();
       let excavatorId = $(this).data('excavator');
 
-      if (!$(this).hasClass('excavators__item-link--active')) {
+      if ($(window).width() >= 768) {
+        if (!$(this).hasClass('excavators__item-link--active') && !$('body').hasClass('excavators-changing')) {
+          excavatorToggles.removeClass('excavators__item-link--active');
+          $(this).addClass('excavators__item-link--active');
+
+          $('body').addClass('excavators-changing');
+          excavatorDetails.removeClass('excavators__details-wrapper--active').fadeOut(250);
+          excavatorDetails.each(function () {
+            let excavator = $(this);
+
+            if (excavator.attr('data-excavator') == excavatorId) {
+              $(this).addClass('excavators__details-wrapper--active');
+              setTimeout(function () {
+                excavator.fadeIn(250);
+              }, 250);
+              setTimeout(function () {
+                $('body').removeClass('excavators-changing');
+              }, 500);
+            };
+          });
+        };
+      } else {
         excavatorToggles.removeClass('excavators__item-link--active');
         $(this).addClass('excavators__item-link--active');
 
-        excavatorDetails.removeClass('excavators__details-wrapper--active').fadeOut(250);
+        excavatorDetails.removeClass('excavators__details-wrapper--active').hide();
         excavatorDetails.each(function () {
           let excavator = $(this);
 
           if (excavator.attr('data-excavator') == excavatorId) {
-            $(this).addClass('excavators__details-wrapper--active');
-            setTimeout(function () {
-              excavator.fadeIn(250);
-            }, 250);
+            $(this).addClass('excavators__details-wrapper--active').show();
+            scrollableWrapper.addClass('excavators__controls--center');
           };
         });
       };
     });
+  });
+
+  excavatorBackBtn.click(function () {
+    scrollableWrapper.removeClass('excavators__controls--center');
+  });
+
+  formBackBtn.click(function () {
+    scrollableWrapper.removeClass('excavators__controls--right').addClass('excavators__controls--center');
   });
 
   //квиз:
@@ -91,7 +144,7 @@ $(document).ready(function() {
     prevArrow: quizPrevBtn,
     nextArrow: quizNextBtn,
     autoPlay: false,
-    adaptiveHeight: true,
+    // adaptiveHeight: true,
     dots: false,
     infinite: false,
     speed: 100,
